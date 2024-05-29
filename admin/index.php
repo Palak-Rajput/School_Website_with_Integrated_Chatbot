c96eeba63c4c8569f701b7fdc74e3a13b60317e8
Palak Rajput <palakrajput8170@gmail.com>
2024-05-29 14:04:24 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -25,14 +25,7 @@
 
                     include "data.php";
                     ?>
-                    <!-- <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol> -->
+        
                 </div>
             </div>
             <!-- /.row -->
