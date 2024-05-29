b754b3251f6442c06c0e26e83556f0c4ad570972
Palak Rajput <palakrajput8170@gmail.com>
2024-05-29 14:09:17 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,31 +1,7 @@
 <?php
-// $db['db_host'] = "localhost";
-// $db['db_user'] = "root";
-// $db['db_pass'] = "";
-// $db['db_name'] = "cms";
 
-// foreach ($db as $key => $value) {
-//     define(strtoupper($key), $value);
-// }
-// $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 $connection = mysqli_connect('localhost', 'root', '' , 'school');
 if (!$connection)
     die("Connection Failed");
-// else
-//     echo "Connected";
 
-// $serverName = "localhost";
-// $userName = "root";
-// $password = "";
-// $dbName = "school";
-
-// try{
-//     $connection = new PDO("mysql:host=$serverName;dbname=$dbName", $userName, $password);
-
-//     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     echo "success";
-// }
-// catch (PDOException $e){
-//     echo "Error: " . $e->getMessage();
-// }
 ?>
