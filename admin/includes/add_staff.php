53eb73351d7a9c81556f8de474c5cb7dfe8baacf
Palak Rajput <palakrajput8170@gmail.com>
2024-05-29 13:58:31 +0530

    Update add_staff.php

--- admin/includes/add_staff.php
+++ admin/includes/add_staff.php
@@ -2,20 +2,9 @@
 
 <?php
 if(isset($_POST['create_staff'])){
-    // global $sno, $name, $designation;
-    // $sno = $_POST['staff_sno'];
     $name = $_POST['staff_name'];
     $designation = $_POST['designation'];
-    // $post_status = $_POST['post_status'];
-    // $post_image = $_FILES['image']['name'];
-    // $post_image_temp = $_FILES['image']['tmp_name'];
-    // $post_tags = $_POST['post_tags'];
-    // $post_content = $_POST['post_content'];
-    // $post_date = date('d-m-y');
-    // $post_comment_count = 4;
-
-    // move_uploaded_file($post_image_temp, "../images/$post_image" );
-
+  
     $query = "INSERT INTO staff( staff_name, designation) ";
 
     $query .= "VALUES('{$name}','{$designation}') ";
