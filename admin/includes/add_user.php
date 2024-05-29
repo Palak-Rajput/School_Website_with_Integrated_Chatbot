2f31f1fa76ff68994f3faacef1b207f510a47e7d
Palak Rajput <palakrajput8170@gmail.com>
2024-05-29 13:59:08 +0530

    Update add_user.php

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -6,15 +6,11 @@ if(isset($_POST['create_user'])){
     $user_firstname = $_POST['user_firstname'];
     $user_lastname = $_POST['user_lastname'];
     $user_role = $_POST['user_role'];
-    // $post_image = $_FILES['image']['name'];
-    // $post_image_temp = $_FILES['image']['tmp_name'];
+
     $username = $_POST['username'];
     $user_email = $_POST['user_email'];
     $user_password = $_POST['user_password'];
-    // $post_date = date('d-m-y');
-    // $post_comment_count = 4;
 
-    // move_uploaded_file($post_image_temp, "../images/$post_image" );
 
     $query = "INSERT INTO users( user_firstname, user_lastname, user_role, username, user_email, user_password) ";
 
@@ -37,9 +33,6 @@ if(isset($_POST['create_user'])){
         <input type="text" class="form-control" name="user_lastname">
     </div>
 
-
-
-    
     
     <div class="form-group">
         <select name="user_role" id="">
@@ -51,11 +44,7 @@ if(isset($_POST['create_user'])){
         </select>
     </div>
 
-    
-    <!-- <div class="form-group">
-        <label for="post_image">Post Image</label>
-        <input type="file" name="image">
-    </div> -->
+ 
     <div class="form-group">
         <label for="post_tags">Username</label>
         <input type="text" class="form-control" name="username">
